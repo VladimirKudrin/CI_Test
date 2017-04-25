5e75fcadd128454283eb96dab15c380ae4d814e5
Vladimir Kudrin <vkudrin@toptrader.group>
2017-04-25 15:17:16 +0300

    Init

---
+++ ExecutablePath/ExecutablePathTest/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Util;
+
+namespace ExecutablePathTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [OneTimeSetUp]
+        public void Init()
+        {
+            ExecutablePath.InitializeFake(string.Empty);
+            Logger.Debug("Trace Init - file : " + ExecutablePath.Combine("Files", "Instruments.txt"));
+        }
+
+        [Test]
+        public void RemoveBulk()
+        {
+        }
+    }
+}
